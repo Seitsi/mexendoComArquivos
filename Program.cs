fbcf19c33aacb833e9d8e24aedc4c6fa28e59167
Seitsi <seitsi2010@gmail.com>
2024-09-09 10:55:25 +0200

    Lendo um arquivo .txt

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Teste autenticacao");
+﻿using System;
+using System.IO;
+
+string path = @"C:\naka\Teste.txt";
+
+FileStream fs = null;
+StreamReader sr = null;
+
+try
+{
+    sr = File.OpenText(path);
+    while (!sr.EndOfStream)
+    {
+        string line = sr.ReadLine();
+        Console.WriteLine(line);
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("ERROR: " + e.Message);
+}
+finally
+{
+    if(sr != null)
+    {
+        sr.Close();
+    }
+}
