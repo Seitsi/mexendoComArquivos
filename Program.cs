00ab3332f627db7f6531ff3af823a8566b8aca62
Seitsi <seitsi2010@gmail.com>
2024-09-16 13:06:32 +0200

    Criando  a leitura de um arquivo, pegando os dados e os transformando em informacao

--- Program.cs
+++ Program.cs
@@ -1,28 +1,38 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
+using Entities;
 
-string path = @"C:\naka\Teste.txt";
-
-FileStream fs = null;
-StreamReader sr = null;
+Console.WriteLine("Insira o caminho inteiro do arquivo: ");
+string sourceFilePath = Console.ReadLine();
 
 try
 {
-    sr = File.OpenText(path);
-    while (!sr.EndOfStream)
+    string[] lines = File.ReadAllLines(sourceFilePath);
+
+    string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+    string targetFolderPath = sourceFolderPath + @"\out";
+    string targetFilePath = targetFolderPath + @"\summary.csv";
+
+    Directory.CreateDirectory(targetFolderPath);
+
+    using (StreamWriter sw = File.AppendText(targetFilePath))
     {
-        string line = sr.ReadLine();
-        Console.WriteLine(line);
+        foreach (string line in lines)
+        {
+
+            string[] fields = line.Split(',');
+            string nome = fields[0];
+            double preco = double.Parse(fields[1], CultureInfo.InvariantCulture);
+            int quantidade = int.Parse(fields[2]);
+
+            Produto prod = new Produto(nome, preco, quantidade);
+
+            sw.WriteLine(prod.Nome + "," + prod.Total().ToString("F2", CultureInfo.InvariantCulture));
+        }
     }
 }
 catch (IOException e)
 {
-    Console.WriteLine("ERROR: " + e.Message);
-}
-finally
-{
-    if(sr != null)
-    {
-        sr.Close();
-    }
+    Console.WriteLine("Ocorreu um erro: " + e.Message);
 }
